51110ddbf174732075ddae76e272e415549ced4d
logos52 <nxlogos@gmail.com>
2026-05-08 09:34:57 +0000

    Top nav: rename home to Index pointing at /notes; fix specificity on home-page top-margin override

--- quartz/components/SiteNav.tsx
+++ quartz/components/SiteNav.tsx
@@ -7,7 +7,7 @@ const SiteNav: QuartzComponent = ({ fileData }: QuartzComponentProps) => {
   return (
     <nav class="site-nav">
       <a href={`${baseDir}/blog`}>Blog</a>
-      <a href={baseDir}>LLM Knowledge Base</a>
+      <a href={`${baseDir}/notes`}>Index</a>
       <a href={`${baseDir}/about`}>About</a>
     </nav>
   )
